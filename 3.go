00ad458ca2b07b4ab43730dd717142cf9cbc69f1
wu-xian <270084782@qq.com>
2018-02-14 22:13:59 +0800

    3

--- 3.go
+++ 3.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("hello go")
+	var slice1 = make([]int, 0)
+	var ptrSlice = &slice1
+	fmt.Println(&ptrSlice)
+	_ = append(slice1, 1)
+	fmt.Println(ptrSlice)
+	_ = append(slice1, 3)
+	fmt.Println(ptrSlice)
+	_ = append(slice1, 5)
+
+	fmt.Println("====================")
+	for _, item := range slice1 {
+		fmt.Print(item)
+		fmt.Print("  ")
+	}
 }
