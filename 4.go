e103bdd43c706d41a486550bdf76acd7dc4723f4
wu-xian <270084782@qq.com>
2018-02-17 22:00:03 +0800

    4

--- 4.go
+++ 4.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	var a int = 65
+	b := string(a)
+	c, _ := strconv.ParseInt("99", 10, 64)
+	d := strconv.Itoa(a)
+	fmt.Println("b", b)
+	fmt.Println("c", c)
+	fmt.Println("d", d)
 }
