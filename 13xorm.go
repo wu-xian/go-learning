d42bc28a14e9e126652bfa372480008e6aa50a5c
wu-xian <123>
2018-03-13 11:31:02 +0800

    13

--- 13xorm.go
+++ 13xorm.go
@@ -8,22 +8,54 @@ import (
 	xorm "github.com/go-xorm/xorm"
 )
 
-type Test struct {
-	Id   int64  `xorm:"'Id'"`
-	Name string `xorm:"'Name'"`
+type test struct {
+	Id    int64  `xorm:"'Id'"`
+	Name  string `xorm:"'Name'"`
+	Value string `xorm:"'Value'"`
 }
 
 func main() {
-	engine, err := xorm.NewEngine("mysql", "root:1234@192.168.1.199:3306/test")
+	engine, err := xorm.NewEngine("mysql", "root:1234@tcp(192.168.1.199:3306)/test")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	t := Test{}
+	t := test{}
 	t.Id = 1
 	result, err := engine.Table("test").Get(&t)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(result)
+	fmt.Println(t)
+
+	//ts := make([]*test, 0)
+	var ts []test
+	engine.Find(&ts)
+	for _, t1 := range ts {
+		fmt.Println("#", t1)
+	}
+
+	t1 := test{
+		Id:    0,
+		Name:  "asd",
+		Value: "999",
+	}
+	t2 := test{
+		Id:    0,
+		Name:  "2222222",
+		Value: "9222222222222222222299",
+	}
+	t3 := test{
+		Id:    0,
+		Name:  "33333333333333",
+		Value: "993333333339",
+	}
+	sess := engine.NewSession()
+	tts := []test{t1, t2, t3}
+	for _, _t := range tts {
+		sess.Insert(_t)
+	}
+	sess.Commit()
+	defer sess.Close()
 }
