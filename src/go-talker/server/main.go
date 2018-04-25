b0dd76b149eedaa4dd4f0a7f3b0d0997fc74c7f3
Gogs <gogs@fake.local>
2018-04-25 23:13:36 +0800

    terminal-base server

---
+++ src/go-talker/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"net"
+
+	cli "github.com/urfave/cli"
+)
+
+const VERSION = "0.0.1"
+
+func main() {
+	app := cli.NewApp()
+	app.Version = VERSION
+	app.UsageText = "go-talker"
+	start := cli.Command{
+		Name:      "start",
+		ShortName: "s",
+		Usage:     "start go-talker",
+		Action:    startAction,
+		HelpName:  "help",
+	}
+	app.Commands = []cli.Command{
+		start,
+	}
+	app.Run(os.Args)
+}
+
+func startAction(ctx cli.Context) {
+	listener, err := net.Listen("tcp", ":34567")
+	if err != nil {
+		panic(err)
+	}
+}
