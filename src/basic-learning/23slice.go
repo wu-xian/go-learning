a957348a9f5aabaa401618d976b75e63df1df7e9
wu-xian <123@mail.com>
2018-05-22 00:42:50 +0800

    clear

--- src/basic-learning/23slice.go
+++ src/basic-learning/23slice.go
@@ -9,7 +9,7 @@ func main() {
 	path := []byte("AAAA/BBBBBBBBB")
 	fmt.Printf("%s %d %d %p \n", path, len(path), cap(path), &path)
 	sepIndex := bytes.IndexByte(path, '/')
-	dir1 := path[:sepIndex]
+	dir1 := path[:sepIndex:4]
 	dir2 := path[sepIndex+1:]
 	fmt.Printf("dir1 => %s %d %d %p \n", string(dir1), len(dir1), cap(dir1), &dir1) //prints: dir1 => AAAA
 	fmt.Printf("dir2 => %s %d %d %p \n", string(dir2), len(dir2), cap(dir2), &dir2) //prints: dir2 => BBBBBBBBB
@@ -24,5 +24,5 @@ func main() {
 	fmt.Println("dir1 =>", string(dir1)) //prints: dir1 => AAAAsuffix
 	fmt.Println("dir2 =>", string(dir2)) //prints: dir2 => uffixBBBB (not ok)
 
-	fmt.Println("new path =>", string(path))
+	fmt.Printf("new path => %s %d %d %p \n", path, len(path), cap(path), &path)
 }
