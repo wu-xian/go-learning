cba6d36df342df50e891cf0df805a9141a44b630
wuxian <wuxian@luojilab.com>
2018-09-30 12:33:11 +0800

    25

---
+++ src/basic-learning/25uintptr.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const PtrSize = 4 << (^uintptr(0) >> 63)
+	fmt.Println(PtrSize)
+	fmt.Println(^uintptr(0))
+	fmt.Println(^uintptr(1))
+	fmt.Println(uint(0x01 << 63))
+}
